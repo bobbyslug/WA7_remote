1072bad190c358db01a163ee99dad6c4317e932b
Christopher He <he.chr@northeastern.edu>
2023-11-01 12:53:22 -0400

    add title

--- WA7_He_8274/ViewController.swift
+++ WA7_He_8274/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Hello test"
         // Do any additional setup after loading the view.
     }
 
